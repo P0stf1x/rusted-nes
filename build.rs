f51807f581fbe7468dff2ada5f8f1940c5d59321
P0stf1x <46126263+P0stf1x@users.noreply.github.com>
2024-09-07 14:27:33 +0300

    Fixed MacOS not linking SDL2

--- build.rs
+++ build.rs
@@ -2,4 +2,7 @@
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rustc-link-search=./include/");
+    if cfg!(target_os = "macos") {
+        println!("cargo:rustc-link-search=framework=/Library/Frameworks");
+    }
 }
