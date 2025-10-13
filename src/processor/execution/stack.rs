c3016e0c7a7dc927fa3cc354c3779e16505a1fa4
Nikita Scherbakov <46126263+P0stf1x@users.noreply.github.com>
2025-10-14 02:57:08 +0300

    Stack refactoring

--- src/processor/execution/stack.rs
+++ src/processor/execution/stack.rs
@@ -1,5 +1,7 @@
-use crate::processor::*;
 use crate::memory::MEM;
+use crate::processor::MemoryMode;
+use crate::processor::instruction::Instruction;
+use crate::CPU;
 
 impl CPU {
     pub fn execute_pha(&mut self, mode: MemoryMode, memory: &mut MEM) {
@@ -34,38 +36,49 @@ impl CPU {
 // PHA IMPL
 impl CPU {
     fn execute_pha_imp(&mut self, memory: &mut MEM) {
-        self.push_stack(self.A.0, memory);
-        self.PC += 1;
+        let inst = Instruction::get_imp(&self, memory);
+        inst.log(&self, "PHA");
+        let value = self.get_a();
+        self.push_stack(value, memory);
+        self.increment_pc(1);
     }
 }
 
 // PHP IMPL
 impl CPU {
     fn execute_php_imp(&mut self, memory: &mut MEM) {
-        let b_temp = self.B;
-        self.B = true;
-        let status: u8 = self.store_status();
-        self.B = b_temp;
-        self.push_stack(status, memory);
-        self.PC += 1;
+        let inst = Instruction::get_imp(&self, memory);
+        inst.log(&self, "PHP");
+        let mut value = self.get_s();
+        value |= 0b_0011_0000;
+        self.push_stack(value, memory);
+        self.increment_pc(1);
     }
 }
 
 // PLA IMPL
 impl CPU {
     fn execute_pla_imp(&mut self, memory: &mut MEM) {
-        self.A = Wrapping(self.pull_stack(memory));
-        self.Z = (self.A.0 & 0b_1111_1111) == 0;
-        self.N = (self.A.0 & 0b_1000_0000) != 0;
-        self.PC += 1;
+        let inst = Instruction::get_imp(&self, memory);
+        inst.log(&self, "PLA");
+        let value = self.pull_stack(memory);
+        self.store_a(value);
+        self.Z = (self.get_a() & 0b_1111_1111) == 0;
+        self.N = (self.get_a() & 0b_1000_0000) != 0;
+        self.increment_pc(1);
     }
 }
 
 // PLP IMPL
 impl CPU {
     fn execute_plp_imp(&mut self, memory: &mut MEM) {
-        let status: u8 = self.pull_stack(memory);
-        self.load_status(status);
-        self.PC += 1;
+        let inst = Instruction::get_imp(&self, memory);
+        inst.log(&self, "PLP");
+        let mut value = self.pull_stack(memory);
+        let current_status = self.get_s() & 0b_0011_0000;
+        value &= 0b_1100_1111;
+        value |= current_status;
+        self.store_s(value);
+        self.increment_pc(1);
     }
 }
